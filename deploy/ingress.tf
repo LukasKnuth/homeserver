3e6d3c4b16d9f754139fc4d83e9226c3721d846f
Lukas Knuth <me@lknuth.dev>
2024-06-24 16:22:45 +0200

    Added Traefik Ingress and Nginx test setup.
    The Domains added to the Ingress rules currently only exist as entries in my local `/etc/hosts` file. We apparently need to run an additional DNS server, as we can't add custom entries to the FritzBox one.

---
+++ deploy/ingress.tf
@@ -0,0 +1,207 @@
+# Docs: https://doc.traefik.io/traefik/routing/providers/kubernetes-ingress/
+# Also: https://doc.traefik.io/traefik/getting-started/quick-start-with-kubernetes/
+
+locals {
+  match_labels = { app = "traefik" }
+}
+
+resource "kubernetes_namespace" "infra" {
+  metadata {
+    name = "infra"
+    labels = {
+      # This is required here to allow Pods _inside_ the namespace to listen to
+      # low-number ports.
+      "pod-security.kubernetes.io/enforce" = "privileged"
+    }
+  }
+}
+
+resource "kubernetes_service_account" "traefik" {
+  metadata {
+    name      = "traefik"
+    namespace = kubernetes_namespace.infra.metadata.0.name
+  }
+}
+
+resource "kubernetes_cluster_role" "traefik-rbac" {
+  metadata {
+    name = "traefik"
+  }
+
+  rule {
+    api_groups = ["extensions", "networking.k8s.io"]
+    resources  = ["ingressclasses", "ingresses"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = ["extensions", "networking.k8s.io"]
+    resources  = ["ingresses/status"]
+    verbs      = ["update"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["services", "endpoints"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  # For some reason, it also needs to be allowed to watch secrets.
+  # Apparently because TlS certs are also stored in secrets (which we don't use).
+  rule {
+    api_groups = [""]
+    resources  = ["secrets"]
+    verbs      = ["list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "traefik-rbac" {
+  metadata {
+    name = "traefik"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.traefik-rbac.metadata.0.name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.traefik.metadata.0.name
+    namespace = kubernetes_namespace.infra.metadata.0.name
+  }
+}
+
+resource "kubernetes_deployment" "traefik" {
+  metadata {
+    name      = "traefik"
+    namespace = kubernetes_namespace.infra.metadata.0.name
+  }
+
+  spec {
+    replicas = 1
+    strategy {
+      # Can't do rolling updates because we're binding to port 80 on the node, which
+      # can't be done twice.
+      type = "Recreate"
+    }
+
+    selector {
+      match_labels = local.match_labels
+    }
+
+    template {
+      metadata {
+        labels = local.match_labels
+      }
+
+      spec {
+        service_account_name = kubernetes_service_account.traefik.metadata.0.name
+
+        container {
+          name  = "traefik"
+          image = "traefik:v3.0"
+          args = [
+            "--providers.kubernetesingress=true",
+            "--entrypoints.web.address=:8000/tcp",
+            "--entrypoints.traefik.address=:9000/tcp",
+            "--api.insecure=true", # enables api/dashboard on "traefik" entrypoint
+            "--ping=true"          # enables /ping on "traefik" entrypoint
+          ]
+
+          port {
+            container_port = 9000
+            protocol       = "TCP"
+            name           = "traefik"
+          }
+
+          port {
+            container_port = 8000
+            protocol       = "TCP"
+            name           = "web"
+            # Expose this on the host directly.
+            # This is the simple workaround for not having "LoadBalancer" type Service support
+            # on bare metal (easily)
+            host_port = 80
+          }
+
+          liveness_probe {
+            http_get {
+              path   = "/ping"
+              port   = "traefik"
+              scheme = "HTTP"
+            }
+          }
+
+          readiness_probe {
+            http_get {
+              path   = "/ping"
+              port   = "traefik"
+              scheme = "HTTP"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_class" "traefik" {
+  metadata {
+    name = "traefik"
+    annotations = {
+      "ingressclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  spec {
+    controller = "traefik.io/ingress-controller"
+  }
+}
+
+# -------- DASHBOARD --------
+# NOTE: Traefik will print an error "Skipping service: no endpoints" for this Service when it
+# restarts. Reason: Traefik itself is the pod/endpoint backing the service. Since we use "Replace"
+# startegy, there is no "Ready"-state Pod for the service yet when it restarts. This causes
+# the log. The problem is resolved as soon as liveness/readiness probes for Traefik itself
+# succeeed and it's added to the service.
+resource "kubernetes_service" "traefik_dashboard" {
+  metadata {
+    name      = "traefik-dashboard"
+    namespace = kubernetes_namespace.infra.metadata.0.name
+  }
+
+  spec {
+    selector = local.match_labels
+    port {
+      port = 9000
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "traefik_dashboard" {
+  metadata {
+    name      = "traefik-dashboard"
+    namespace = kubernetes_namespace.infra.metadata.0.name
+  }
+
+  spec {
+    rule {
+      host = "traefik.rpi"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.traefik_dashboard.metadata.0.name
+              port {
+                number = 9000
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- deploy/main.tf
+++ deploy/main.tf
@@ -35,3 +35,47 @@ resource "kubernetes_deployment" "nginx" {
     }
   }
 }
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name      = "nginx-test"
+    namespace = kubernetes_namespace.apps.metadata.0.name
+  }
+
+  spec {
+    selector = { app = "nginx" }
+
+    type = "ClusterIP"
+
+    port {
+      port = 80
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "nginx" {
+  metadata {
+    name      = "nginx-test"
+    namespace = kubernetes_namespace.apps.metadata.0.name
+  }
+
+  spec {
+    rule {
+      host = "test.rpi"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.nginx.metadata.0.name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
