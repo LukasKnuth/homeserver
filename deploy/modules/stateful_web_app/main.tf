2e1a5aedf266a39931e208ba7a727b648f6e389a
Lukas Knuth <me@lknuth.dev>
2024-06-25 20:57:34 +0200

    Initial version of the stateful web-app module.
    Currently it's not really stateful at all, but a good basis for creating a web application. Some areas for improvement are noted.

---
+++ deploy/modules/stateful_web_app/main.tf
@@ -0,0 +1,103 @@
+locals {
+  match_labels = {
+    "app.kubernetes.io/name"       = var.name
+    "app.kubernetes.io/managed-by" = "terraform"
+  }
+}
+
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = local.match_labels
+    }
+
+    template {
+      metadata {
+        labels = local.match_labels
+      }
+
+      spec {
+        container {
+          name  = var.name # generate a better name?
+          image = var.image
+          # TODO how to allow "args" overriding but use default if not specified?
+
+          port {
+            container_port = var.expose_port
+            protocol       = "TCP"
+          }
+
+          dynamic "liveness_probe" {
+            for_each = var.liveness_get_path == null ? [] : [1]
+            content {
+              http_get {
+                path   = var.liveness_get_path
+                port   = var.expose_port
+                scheme = "HTTP"
+              }
+            }
+          }
+
+          dynamic "readiness_probe" {
+            for_each = var.readiness_get_path == null ? [] : [1]
+            content {
+              http_get {
+                path   = var.readiness_get_path
+                port   = var.expose_port
+                scheme = "HTTP"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "web_service" {
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+  }
+
+  spec {
+    selector = local.match_labels
+    port {
+      port = var.expose_port
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "web_ingress" {
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+  }
+
+  spec {
+    rule {
+      host = var.fqdn
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.web_service.metadata.0.name
+              port {
+                number = kubernetes_service.web_service.spec.0.port.0.port
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ deploy/modules/stateful_web_app/variables.tf
@@ -0,0 +1,45 @@
+variable "name" {
+  type        = string
+  description = "The name of the application."
+}
+
+variable "image" {
+  type        = string
+  description = "The image descriptor to fetch and run the App"
+}
+
+variable "expose_port" {
+  type        = number
+  description = "The port to expose to the world for interacting with the application."
+}
+
+variable "fqdn" {
+  type        = string
+  description = "The FQDN the application should be avilable under"
+}
+
+variable "sqlite_file_name" {
+  type        = string
+  description = "The name of the SQLite file which stores the application state."
+}
+
+# ------------ Overridable --------------
+variable "namespace" {
+  type        = string
+  description = "The namespace to deploy ALL application resources into."
+  default     = "apps"
+}
+
+variable "readiness_get_path" {
+  type        = string
+  nullable    = true
+  default     = null
+  description = "The URL to HTTP GET for the Readiness Probe"
+}
+
+variable "liveness_get_path" {
+  type        = string
+  nullable    = true
+  default     = null
+  description = "The URL to HTTP GET for the Liveness Probe"
+}
