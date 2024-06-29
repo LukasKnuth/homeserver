420a365e53ec88a8f5003e475da3e0a91b67e2de
Lukas Knuth <me@lknuth.dev>
2024-06-29 17:10:34 +0200

    Added Wallabag as first test app.

--- deploy/main.tf
+++ deploy/main.tf
@@ -8,4 +8,19 @@ resource "kubernetes_namespace" "apps" {
   }
 }
 
+module "testapp" {
+  source      = "./modules/stateful_web_app"
+  name        = "wallabag"
+  namespace   = local.namespace
+  image       = "wallabag/wallabag:2.6.9"
+  expose_port = 80
+  env = {
+    "SYMFONY__ENV__DOMAIN_NAME"     = "http://test.rpi"
+    "SYMFONY__ENV__DATABASE_DRIVER" = "pdo_sqlite"
+  }
+  fqdn           = "test.rpi"
+  sqlite_path    = "/opt/wallabag/data/wallabag.sqlite"
+  s3_secret_name = kubernetes_secret_v1.litestream_config.metadata.0.name
+  s3_url         = minio_s3_bucket.litestream_destination.bucket
+}
 
