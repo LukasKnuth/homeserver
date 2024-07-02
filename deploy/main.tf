3f18ca2d5c931aea8e0bcf04e78f82dfeaf39ab8
Lukas Knuth <me@lknuth.dev>
2024-07-02 13:55:49 +0200

    Updated wallabag domain to more suitable one.

--- deploy/main.tf
+++ deploy/main.tf
@@ -15,10 +15,10 @@ module "testapp" {
   image       = "wallabag/wallabag:2.6.9"
   expose_port = 80
   env = {
-    "SYMFONY__ENV__DOMAIN_NAME"     = "http://test.rpi"
+    "SYMFONY__ENV__DOMAIN_NAME"     = "http://wallabag.rpi"
     "SYMFONY__ENV__DATABASE_DRIVER" = "pdo_sqlite"
   }
-  fqdn           = "test.rpi"
+  fqdn           = "wallabag.rpi"
   sqlite_path    = "/opt/wallabag/data/wallabag.sqlite"
   s3_secret_name = kubernetes_secret_v1.litestream_config.metadata.0.name
   s3_url         = minio_s3_bucket.litestream_destination.bucket
