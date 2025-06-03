4a3db7d251c14c4df8f8c9396020c3a689e977ea
Lukas Knuth <me@lknuth.dev>
2025-06-03 18:32:42 +0200

    Use ENV variable for `replica-verify` image.
    This allows us to specify all information in the same way. It also allows the container easily be started interactively by overwriting `CMD` with `-l` for an interactive Fish session.

--- maintenance/replica-verify/entrypoint.fish
+++ maintenance/replica-verify/entrypoint.fish
@@ -1,10 +1,17 @@
-set -l db_name $argv[1]
-set -l db_path "/app/db.sqlite"
+# Verifies the SQLite replication by restoring the newest version from the backup
+# and running and SQLite integrity check on the result.
+# Environment variables:
+# - `APP_DB_PATH` set to the full path of the apps database. This is used to identify
+#   which datbase to restore and must be the EXACT same as in the Litestream sidecar.
+# - `HEALTHCHECKS_IO_URL` the `hc-ping.com` URL with a UUID to call once the verification
+#   is completed and has succeeded/failed. The output will be posted there as well.
+
+set -l local_db "/app/db.sqlite"
 
 # Restore database from newest replica generation
 # If succeeds, verify the integrity of the restored database
-set -l log (litestream restore -o $db_path $db_name 2>&1; and sqlite3 $db_path "PRAGMA integrity_check" 2>&1)
+set -l log (litestream restore -o $local_db $APP_DB_PATH 2>&1; and sqlite3 $local_db "PRAGMA integrity_check" 2>&1)
 
 # Report status and log to healthcheck.io
-set -l url "https://hc-ping.com/$HEALTHCHECKS_IO_UUID/$status"
+set -l url "$HEALTHCHECKS_IO_URL/$status"
 curl -m 20 --retry 5 --data-raw "$(string split0 $log)" $url
